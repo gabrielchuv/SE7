6d7cd83eca2a6a8a9f485162e744cf10c63748bf
Alex Digpal-Race <alex.digpalrace@gmail.com>
2021-04-04 11:00:47 +0100

    building ng service for bins and foods

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoodViewComponent } from './search/food-view/food-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FoodViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FoodViewComponent } from './search/food-view/food-view.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoodViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
