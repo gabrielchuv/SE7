550c29f24ae773177bc9743055e67ce40f97e4b4
zainzzkk <zainzzkk@gmail.com>
2021-04-07 15:04:40 +0100

    fixed the commit paths for new file structure

--- frontend/src/app/common/modules/app.module.ts
+++ frontend/src/app/common/modules/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../components/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FoodViewComponent } from './search/food-view/food-view.component';
+import { FoodViewComponent } from '../../search/food-view/food-view.component';
 
 
 @NgModule({
--- frontend/src/app/search/food-view/food-view.component.ts
+++ frontend/src/app/search/food-view/food-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import Fooditem from 'src/app/models/fooditem';
-import { SearchService } from 'src/app/search.service';
+import { SearchService } from 'src/app/common/services/search.service';
 import { FormControl } from '@angular/forms';
 import { Observable, observable } from 'rxjs';
 import { stringify } from '@angular/compiler/src/util';
@@ -13,38 +13,39 @@ import { stringify } from '@angular/compiler/src/util';
 })
 export class FoodViewComponent implements OnInit {
 
-  //form control instance used for searching
-  searchControl = new FormControl('');    //intial value is empty string as this will be updated when user enters something to search for...
-  //array of food items, initially empty
+  // form control instance used for searching
+  // tslint:disable-next-line:max-line-length
+  searchControl = new FormControl('');    // intial value is empty string as this will be updated when user enters something to search for...
+  // array of food items, initially empty
   fooditems: Fooditem[] = [];
-  //food item (on click display info)
+  // food item (on click display info)
   fooditem: Fooditem | undefined;
-  //for saving fooditem name user is viewing in further detail
-  userSearchFood: string = "";
-  foodName: string = "";
+  // for saving fooditem name user is viewing in further detail
+  userSearchFood = '';
+  foodName = '';
 
-  //link a search service instance on creation
+  // link a search service instance on creation
   constructor(
-    private searchService: SearchService,       //for using the seearch service we created
-    private route: ActivatedRoute,              //for getting the current route
-    private router: Router                      //for redirecting the user to another route
+    private searchService: SearchService,       // for using the seearch service we created
+    private route: ActivatedRoute,              // for getting the current route
+    private router: Router                      // for redirecting the user to another route
   ) { }
 
-  //prevents access to ng things before its laoded on the page
+  // prevents access to ng things before its loaded on the page
   ngOnInit() {
     this.searchControl.valueChanges.subscribe((word: string) => {
       this.userSearchFood = word;
       console.log(`search word changing: ${word}`);
     });
   }
-  //TODO: add error handling for non existant food items
+  // TODO: add error handling for non existant food items
   onSearch() {
-    if (this.userSearchFood == "") {
-      //subscription displays database results and will update if there are any changes to the database files
+    if (this.userSearchFood === '') {
+      // subscription displays database results and will update if there are any changes to the database files
       this.searchService.getFoods().subscribe((fooditems: any) => {
          this.fooditems = fooditems;
-        
-      })
+
+      });
     } else {
       this.searchService.getFood(this.userSearchFood).subscribe((fooditem: any) => {
         next: {
@@ -52,18 +53,18 @@ export class FoodViewComponent implements OnInit {
             this.fooditems.push(fooditem);
         }
         error: {
-          //clear the array
+          // clear the array
           this.fooditems = [];
-          //push into it a new error food item
-          this.fooditems.push({ _id: "error", name:"Item not found: try another one", mass: "error", cost: "error", category: "go away" });
+          // push into it a new error food item
+          this.fooditems.push({ _id: 'error', name: 'Item not found: try another one', mass: 'error', cost: 'error', category: 'go away' });
         }
-      })
+      });
     }
   }
 
   onMoreDetails(food: string) {
 
-      if (!food) return;
+      if (!food) { return; }
       this.searchService.getFood(food).subscribe((fooditem: any) => this.fooditem = fooditem);
   }
 }
