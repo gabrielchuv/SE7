0107acc37ca17eca530c5818ec27abfbae7aecb2
Jessica <sungjhj@gmail.com>
2021-04-08 13:35:24 +0100

    main-page background set

--- frontend/src/app/common/modules/app-routing.module.ts
+++ frontend/src/app/common/modules/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FoodViewComponent } from '../../search/food-view/food-view.component';
+import { MainPageComponent } from '../../main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'search', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
+  { path: 'test', component: MainPageComponent},
   { path: 'search', component: FoodViewComponent },
   { path: 'search/moredetails/:foodname', component: FoodViewComponent },
 ];
