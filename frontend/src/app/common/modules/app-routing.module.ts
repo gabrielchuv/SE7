9f9f2b9fa3959aff680d52457cb65a3d68345355
Jessica <sungjhj@gmail.com>
2021-04-08 16:17:21 +0100

    main-page boilerplate for search bar

--- frontend/src/app/common/modules/app-routing.module.ts
+++ frontend/src/app/common/modules/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FoodViewComponent } from '../../search/food-view/food-view.component';
 import { MainPageComponent } from '../../main-page/main-page.component';
+import { SearchBarComponent } from '../../main-page/search-bar/search-bar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'search', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
-  { path: 'test', component: MainPageComponent},
+  { path: 'test', component: SearchBarComponent},
   { path: 'search', component: FoodViewComponent },
   { path: 'search/moredetails/:foodname', component: FoodViewComponent },
 ];
