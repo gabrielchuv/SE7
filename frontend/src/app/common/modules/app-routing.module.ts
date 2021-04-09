ed2fb43259ff7327918de601c608b29279ac17a1
jessicaksung <jessica.k.sung@gmail.com>
2021-04-09 17:16:18 +0100

    removed search-bar component from routing

--- frontend/src/app/common/modules/app-routing.module.ts
+++ frontend/src/app/common/modules/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from '../../main-page/main-page.component';
-import { SearchBarComponent } from '../../main-page/search-bar/search-bar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'search', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
