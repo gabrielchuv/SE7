175ef5778ce3378dad2a480badf497bacc5ef8b0
jessicaksung <jessica.k.sung@gmail.com>
2021-04-10 19:10:57 +0100

    adjusting routing so it goes to main-page not search (old version)

--- frontend/src/app/common/modules/app-routing.module.ts
+++ frontend/src/app/common/modules/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StatsPageComponent } from 'src/app/stats-page/stats-page.component';
-import { FoodViewComponent } from '../../search/food-view/food-view.component';
 import { LandingPageComponent } from '../../landing-page/landing-page.component';
 import { MainPageComponent } from './../../main-page/main-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'search', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
-  { path: 'search', component: FoodViewComponent },
-  { path: 'search/moredetails/:foodname', component: FoodViewComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
   { path: 'stats', component: StatsPageComponent },
   { path: 'main', component: MainPageComponent}
 
