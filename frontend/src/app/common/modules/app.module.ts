1aa5e69f61e5888910e427704558dd50f134d4ab
ng0c <anitatnguyen@outlook.com>
2021-04-13 14:59:35 +0100

    Implemented edit quantity to receipt

--- frontend/src/app/common/modules/app.module.ts
+++ frontend/src/app/common/modules/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../components/app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { FoodViewComponent } from '../../search/food-view/food-view.component';
 import { LandingPageComponent} from '../../landing-page/landing-page.component';
 import { MainPageComponent } from './../../main-page/main-page.component';
@@ -23,7 +23,8 @@ import { SearchBarComponent} from './../../main-page/search-bar/search-bar.compo
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
