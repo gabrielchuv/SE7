7671307e38820d1beab879e6eef97a5f754b397e
ng0c <anitatnguyen@outlook.com>
2021-04-15 13:12:40 +0100

    implemented item running total

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -11,6 +11,8 @@ import Bin from '../models/bin';
 
 export class MainPageComponent implements OnInit {
   binList: Bin[] = [];
+  runningTotal: number = 0;
+
   //link a search service instance on creation
   constructor(
     private searchService: SearchService,       //for using the seearch service we created
@@ -19,28 +21,36 @@ export class MainPageComponent implements OnInit {
   ) { }
   //prevents access to ng things before its loaded on the page
   ngOnInit() {
+
   }
+
   addBinEntry(foodName: string) {
     console.log(`adding to the bin: ${foodName}`);
     this.binList.push(new Bin(foodName, "1"));
+    this.runningTotal++;
   }
+
   decrementQuantity(bin: any) {
     //parse to int
     var asNumber = parseInt(bin.quantity);
     //if its not 0
     if (asNumber > 0) {
       asNumber--;
+      this.runningTotal--;
     }
     //turn back to string and set it
     bin.quantity = asNumber.toString();
     console.log(`decrementing amount to: ${bin.quantity}`);
   }
+
   incrementQuantity(bin: any) {
     var asNumber = parseInt(bin.quantity);
     asNumber++;
+    this.runningTotal++;
     bin.quantity = asNumber.toString();
     console.log(`incrementing amount to: ${bin.quantity}`);
   }
+
   toStats() {
     this.router.navigate(['../stats'], { relativeTo: this.route });
   }
