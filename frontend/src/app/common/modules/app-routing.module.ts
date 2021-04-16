01a5bd3509d4ff41358da3be13e48e15e19b0a6e
zainzzkk <zainzzkk@gmail.com>
2021-04-16 18:53:23 +0100

    navbar added - needs styling next

--- frontend/src/app/common/modules/app-routing.module.ts
+++ frontend/src/app/common/modules/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StatsPageComponent } from 'src/app/stats-page/stats-page.component';
 import { LandingPageComponent } from '../../landing-page/landing-page.component';
-import { MainPageComponent } from './../../main-page/main-page.component';
+import { MainPageComponent } from '../../main-page/main-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
