533ee851570d4a0a0368adcafb0d8379fd67b799
ng0c <anitatnguyen@outlook.com>
2021-04-20 15:34:48 +0100

    added footer with basic styling

--- frontend/src/app/common/modules/app-routing.module.ts
+++ frontend/src/app/common/modules/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { StatsPageComponent } from 'src/app/stats-page/stats-page.component';
 import { LandingPageComponent } from '../../landing-page/landing-page.component';
 import { MainPageComponent } from '../../main-page/main-page.component';
+import { FooterComponent } from './../../footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },    //full makes sure its goes to exactly search and not search/something
   { path: 'home', component: LandingPageComponent },
   { path: 'stats', component: StatsPageComponent },
-  { path: 'main', component: MainPageComponent}
+  { path: 'main', component: MainPageComponent},
+  { path: 'footer', component: FooterComponent}
 
 ];
 
