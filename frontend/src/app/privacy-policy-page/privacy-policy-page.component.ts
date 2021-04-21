162724d36feadc5517c2899c1540a76fbe41baa4
Gabriel <gabriel.chu.v@hotmail.com>
2021-04-21 17:18:29 +0100

    Added privacy policy component

---
+++ frontend/src/app/privacy-policy-page/privacy-policy-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy-page',
+  templateUrl: './privacy-policy-page.component.html',
+  styleUrls: ['./privacy-policy-page.component.css']
+})
+export class PrivacyPolicyPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
