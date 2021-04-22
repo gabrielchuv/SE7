76527c3a775761054e04a645aa4813961e229d99
Gabriel <gabriel.chu.v@hotmail.com>
2021-04-22 20:43:53 +0100

    Trying db atlas

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const mongoose = require('./backend/node_modules/mongoose');
 /* SEMI NEW */
-/*const url = `mongodb+srv://gabrielchuv:qZ7nKcff5pLb67vQ@food-waste-app.esxbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;*/
+const url = `mongodb+srv://gabrielchuv:qZ7nKcff5pLb67vQ@food-waste-app.esxbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 /* NEW NEW  */
 /*const url = process.env.MONGODB_URI;*/
 /* NEW */
-/*const options={
+const options={
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true 
-}*/
+}
 
 
 const Fooditem = require('./backend/database/models/fooditem');
