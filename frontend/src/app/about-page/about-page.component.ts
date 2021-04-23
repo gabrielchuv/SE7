450bf11f3b509e3eed56546e7a253a72bd3987b8
ng0c <anitatnguyen@outlook.com>
2021-04-23 15:19:30 +0100

    final formatting and linked home page with nav

--- frontend/src/app/about-page/about-page.component.ts
+++ frontend/src/app/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-page',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class AboutPageComponent implements OnInit {
   panelOpenState = false;
   
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onNavClick() {
+    this.router.navigate(['../home'], {relativeTo: this.route});
   }
 
 }
