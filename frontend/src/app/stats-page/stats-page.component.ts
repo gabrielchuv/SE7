b91822c8b4c10b7a7e5be926386a758aedd81cd3
Gabriel <gabriel.chu.v@hotmail.com>
2021-04-26 19:26:21 +0100

    copy link button

--- frontend/src/app/stats-page/stats-page.component.ts
+++ frontend/src/app/stats-page/stats-page.component.ts
@@ -53,6 +53,20 @@ export class StatsPageComponent implements OnInit {
     })
   }
 
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
   onNavClick() {
     this.router.navigate(['../home'], {relativeTo: this.route});
   }
