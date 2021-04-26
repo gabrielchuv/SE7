317caefaaaf31a19600a3069bdb1ec9bd8e0a673
Alex Digpal-Race <alex.digpalrace@gmail.com>
2021-04-26 14:45:24 +0100

    added yearly forecast and messaging service

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SearchService } from 'src/app/common/services/search.service';
 import Bin from '../models/bin';
+import { MessageService } from '../common/services/message.service';
+import Fooditem from '../models/fooditem';
 
 @Component({
   selector: 'app-main-page',
@@ -12,12 +14,15 @@ import Bin from '../models/bin';
 export class MainPageComponent implements OnInit {
   binList: Bin[] = [];
   runningTotal: number = 0;
+  fooditemCost: number = 0;
+  binTotal: number = 0;
 
   //link a search service instance on creation
   constructor(
     private searchService: SearchService,       //for using the seearch service we created
     private route: ActivatedRoute,              //for getting the current route
-    private router: Router                      //for redirecting the user to another route
+    private router: Router,                      //for redirecting the user to another route
+    private messageService: MessageService       //for saving the yearly forecast of the bin
   ) { }
 
   //prevents access to ng things before its loaded on the page
@@ -25,12 +30,11 @@ export class MainPageComponent implements OnInit {
   }
 
   addBinEntry(foodName: string) {
-    console.log(`adding to the bin: ${foodName}`);
     this.binList.push(new Bin('1', foodName, "1"));
     this.runningTotal++;
   }
 
-  //decrement quanity of the passed bin
+  //decrement quantity of the passed bin
   decrementQuantity(bin: any) {
     //parse to int
     var asNumber = parseInt(bin.quantity);
@@ -41,26 +45,25 @@ export class MainPageComponent implements OnInit {
     }
     //turn back to string and set it
     bin.quantity = asNumber.toString();
-    console.log(`decrementing amount to: ${bin.quantity}`);
   }
 
-  //increment quanity of the passed bin
+  //increment quantity of the passed bin
   incrementQuantity(bin: any) {
     var asNumber = parseInt(bin.quantity);
     asNumber++;
     this.runningTotal++;
     bin.quantity = asNumber.toString();
-    console.log(`incrementing amount to: ${bin.quantity}`);
   }
 
-
-  // send binList data to backend
+  // send binList data to backend and pass it to stats page for yearly forecast
   toStats() {
     console.log("redirecting to stats page and sending data...");
     for (let i = 0; i < this.binList.length; i++) {
       this.searchService.createBin(this.binList[i].usrID!, this.binList[i].food!, this.binList[i].quantity!)
-        .subscribe((res) => console.log(`added bin: ${res} to database`));
+        .subscribe();
     }
+    //send binList to stats page to calculate yearly forecast
+    this.messageService.setBinList(this.binList);
     this.router.navigate(['../stats'], { relativeTo: this.route });
   }
 
--- frontend/src/app/stats-page/stats-page.component.ts
+++ frontend/src/app/stats-page/stats-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { SearchService } from 'src/app/common/services/search.service';
+import { MessageService } from '../common/services/message.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import Bin from '../models/bin';
 
@@ -12,44 +13,87 @@ import Bin from '../models/bin';
 
 export class StatsPageComponent implements OnInit {
   binEntryTotal: number = 0;
-  totalCost: number = 0;
+  binTotal: number = 0;
   i: number = 0;
+  binList: Bin[] = [];
 
   constructor(
     private searchService: SearchService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService
   ) { }
-    
-  
-  ngOnInit() {
-    this.searchService.getBins().subscribe((bins: any) => {
-      if (bins != null) {
-        //go through each entry in bins
-        bins.forEach((binEntry: any) => {
-          //search food items with the food name
-          this.searchService.getFood(binEntry.food!).subscribe((foods) => {
-            //foods is an array but there SHOULD only be one food item in it, so get its zeroth elements cost 
-            //multiply it by the quantity and save the total BIN ENTRY cost
-          
-            this.binEntryTotal = parseFloat(foods[0].cost!) * parseInt(binEntry.quantity!);
-            
-            //add the bin entry cost to the total
-            this.totalCost += this.binEntryTotal;
-        
-            console.log(`bin entry: ${this.i++} total bin entry cost: ${this.binEntryTotal.toFixed(2)}`);
-          })
-        });
 
-        console.log(bins); //for debugging
+  //on page build
+  ngOnInit() {
+    //get the total cost from the messaging service
+    this.binList = this.messageService.getBinList();
+    this.binList.forEach((binEntry) => {
+      if (binEntry != null) {
+        //get the foods cost from the database
+        this.searchService.getFood(binEntry.food!)
+          .subscribe((fooditem: any) => {
+            //get the total cost of the binEntry (= food.cost x bin.quantity)
+            this.binEntryTotal = parseFloat(fooditem[0].cost) * parseInt(binEntry.quantity!);
+            console.log(`Bin entry: food:${binEntry.food} cost:${fooditem[0].cost} quantity=${binEntry.quantity!}`);
+            //calculate a running total for the year
+            this.binTotal += this.binEntryTotal * 52;
+          })
       }
     })
   }
 
   onNavClick() {
-    this.router.navigate(['../home'], {relativeTo: this.route});
+    this.router.navigate(['../home'], { relativeTo: this.route });
   }
-
-
-
 }
+
+//get the total cost of the users binS:
+/*this.searchService.getBins().subscribe((bins: any) => {
+  if (bins != null) {
+    //go through each entry in bins
+    bins.forEach((binEntry: any) => {
+      //search food items with the food name
+      this.searchService.getFood(binEntry.food!).subscribe((foods) => {
+        //foods is an array but there SHOULD only be one food item in it, so get its zeroth elements cost
+        //multiply it by the quantity and save the total BIN ENTRY cost
+
+        this.binEntryTotal = parseFloat(foods[0].cost!) * parseInt(binEntry.quantity!);
+
+        //add the bin entry cost to the total
+        this.totalCost += this.binEntryTotal;
+
+        console.log(`bin entry: ${this.i++} total bin entry cost: ${this.binEntryTotal.toFixed(2)}`);
+      })
+    });
+
+    console.log(bins); //for debugging
+  }
+})*/
+
+/*
+calculateTotal() {
+    let calculation = new Promise((resolve, reject) => {
+      //go through each food item in list
+      this.binList.forEach((binEntry: any) => {
+        console.log(binEntry.food);
+        if (binEntry != null) {
+          //get the foods cost from the database
+          this.searchService.getFood(binEntry.food!)
+            .subscribe((fooditem: any) => {
+              this.fooditemCost = parseFloat(fooditem[0].cost);
+              //get the total cost of the binEntry (= food.cost x bin.quantity)
+              this.binTotal += this.fooditemCost * parseInt(binEntry.quantity);
+            })
+          }
+        })
+      })
+      calculation.then(() => {
+        //multiply binTotal by 52 (weeks) to get the yearly forecast
+        this.binTotal *= 52;
+        //save yearly forecast to messageService
+        this.messageService.setBinTotal(this.binTotal);
+        console.log(`yearly forecast: ${this.binTotal}`);
+    })
+  }
+*/
