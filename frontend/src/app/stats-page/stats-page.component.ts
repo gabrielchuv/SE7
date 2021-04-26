f454a1c33f7a701b616fbdf7b01dfcbb00ab3fcf
Gabriel <gabriel.chu.v@hotmail.com>
2021-04-26 19:33:49 +0100

    copy link button

--- frontend/src/app/stats-page/stats-page.component.ts
+++ frontend/src/app/stats-page/stats-page.component.ts
@@ -56,13 +56,7 @@ export class StatsPageComponent implements OnInit {
   copyMessage(val: string){
     const selBox = document.createElement('textarea');
     selBox.style.position = 'fixed';
-    selBox.style.left = '0';
-    selBox.style.top = '0';
-    selBox.style.opacity = '0';
     selBox.value = val;
-    document.body.appendChild(selBox);
-    selBox.focus();
-    selBox.select();
     document.execCommand('copy');
     document.body.removeChild(selBox);
   }
