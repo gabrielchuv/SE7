e42129a75255df5c094036cd4bc2751e6558aebf
Alex Digpal-Race <alex.digpalrace@gmail.com>
2021-04-28 13:50:05 +0100

    sprint reviews and scrum

--- frontend/src/app/main-page/main-page.component.spec.ts
+++ frontend/src/app/main-page/main-page.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import Bin from '../models/bin';
 import { MainPageComponent } from './main-page.component';
+
+
+//test increment bin quantity increments the running total
+//test decerement bin quantity decrements the running total
+//test a food item is emitted when clicked on from the search results
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
@@ -19,7 +24,22 @@ describe('MainPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('increment quanitity should increment the running total', () => {
+    expect(component.runningTotal).toEqual(0);
+    component.incrementQuantity(new Bin('01', 'apple', '0'));
+    expect(component.runningTotal).toEqual(1);
+    component.incrementQuantity(new Bin('01', 'apple', '1'));
+    expect(component.runningTotal).toEqual(2);
   });
+
+  it('decrement quantity should decrement the running total', () => {
+    expect(component.runningTotal).toEqual(2);
+    component.decrementQuantity(new Bin('01', 'apple', '2'));
+    expect(component.runningTotal).toEqual(1);
+    component.decrementQuantity(new Bin('01', 'apple', '0'));
+    expect(component.runningTotal).toEqual(0);
+    component.decrementQuantity(new Bin('01', 'apple', '0'));
+    //shouldnt go below 0
+    expect(component.runningTotal).toEqual(0);
+  })
 });
