6b2bcc999a96f30b873938659d273aecf3729cd2
Gabriel <gabriel.chu.v@hotmail.com>
2021-05-05 17:30:33 +0100

    deleted comments

--- frontend/src/app/stats-page/stats-page.component.ts
+++ frontend/src/app/stats-page/stats-page.component.ts
@@ -49,6 +49,7 @@ export class StatsPageComponent implements OnInit {
             console.log(`Bin entry: food:${binEntry.food} cost:${fooditem[0].cost} quantity=${binEntry.quantity!}`);
             //calculate a running total for the year
             this.binTotal += this.binEntryTotal * 52;
+            //conditional text to compare forecaste with average money wasted in the UK
             if(this.binTotal > 356){
               this.displayText = `That is £${Math.round(this.binTotal - 356)} more than the average person in the UK.`;
             }
